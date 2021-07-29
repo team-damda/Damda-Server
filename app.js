931eff346d74a698ff51e417a9079b418f3e798a
Jin Lee <leejin21c@gmail.com>
2021-07-29 23:57:04 +0900

    [develop] build: app.js 재구성 및 dotenv install - 진

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// node_modules 에 있는 express 관련 파일을 가져온다.
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+    res.send("hello world");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 3000 포트로 서버 오픈
+app.listen(3000, function () {
+    console.log("start! express server on port 3000");
 });
 
-module.exports = app;
+// 이제 터미널에 nodemon src/app.js 를 입력해보자.
