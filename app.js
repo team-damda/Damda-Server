25912162aff28dd45454616765ea42a5fc58ae48
Jin Lee <leejin21c@gmail.com>
2021-09-11 23:44:06 +0900

    fix(ec2 rest): Error: Failed to lookup view index  FIX

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var allowCrossDomain = function (req, res, next) {
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 };
-app.set("view engine", "ejs"); //'ejs'탬플릿을 엔진으로 한다.
-app.set("views", path.join(__dirname, "views")); //폴더, 폴더경로 지정
-app.use(express.static(path.join(__dirname + "../public")));
+app.set("view engine", "jade");
+app.set("views", path.join(__dirname, "./views")); //폴더, 폴더경로 지정
+app.use(express.static(path.join(__dirname + "./public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
