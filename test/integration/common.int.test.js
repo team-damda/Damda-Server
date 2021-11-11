bde15f2f2157e26b7ea0635fc0bf015ea0e7ccc6
JinLee <41279487+leejin21@users.noreply.github.com>
2021-11-11 16:17:47 +0900

    [Feature/common] 테스트 코드 수정(history 매번 수정해야 했던 문제 FIX) (#11)
    * feat(status): 테스트 코드 알맞게 수정(history 매번 안 맞던 부분 FIX)
    
    * fix(holding): 테스트 모듈에서 매번 데이터 수정해줘야 해 그냥 삭제함.

--- test/integration/common.int.test.js
+++ test/integration/common.int.test.js
@@ -1,47 +1,70 @@
-// const request = require("supertest");
-// const { sequelize } = require("../../models");
-// const app = require("../../app");
-// const successMeta = require("../../modules/success-meta");
-// const errorMeta = require("../../modules/error-meta");
+const request = require("supertest");
+const { sequelize } = require("../../models");
+const app = require("../../app");
+const successMeta = require("../../modules/success-meta");
+const errorMeta = require("../../modules/error-meta");
+const { getHistory } = require("../../modules/test-modules");
 
-// beforeAll(async () => {
-//     await sequelize.sync();
-// });
+beforeAll(async () => {
+    await sequelize.sync();
+});
 
-// describe("GET /common/containStocks", function () {
-//     it("user id Users에 존재하는 정상적인 get의 경우", function (done) {
-//         request(app)
-//             .get("/common/holdingStocks")
-//             .query({ UserId: 1 })
-//             .set("Accept", "application/json")
-//             .expect((res) => {
-//                 // console.log(res.body);
-//                 expect(res.body).toStrictEqual({
-//                     status: 200,
-//                     success: true,
-//                     message: successMeta["SUC-COMMON-0001"].message,
-//                     data: [
-//                         {
-//                             currentPrice: 5000,
-//                             marketType: "코스피",
-//                             stockId: "AA",
-//                             stockName: "밤새는거극혐",
-//                             totCnt: 2,
-//                             totProfitLoss: 8000,
-//                             totProfitLossRate: 400,
-//                         },
-//                         {
-//                             currentPrice: 5000,
-//                             marketType: "코스닥",
-//                             stockId: "BB",
-//                             stockName: "라이언",
-//                             totCnt: 3,
-//                             totProfitLoss: 9000,
-//                             totProfitLossRate: 150,
-//                         },
-//                     ],
-//                 });
-//             })
-//             .expect(200, done);
-//     });
-// });
+describe("GET /common/status", function () {
+    it("user id Users에 존재하는 정상적인 get의 경우", function (done) {
+        request(app)
+            .get("/common/status")
+            .query({ UserId: 1 })
+            .set("Accept", "application/json")
+            .expect((res) => {
+                expect(res.body).toStrictEqual({
+                    status: 200,
+                    success: true,
+                    message: successMeta["SUC-COMMON-0001"].message,
+                    data: {
+                        nickname: "테스트",
+                        history: getHistory(new Date()),
+                        deposit: 10000000,
+                        containStockAsset: 0,
+                    },
+                });
+            })
+            .expect(200, done);
+    });
+    it("user id 존재하지만 ContainStock에 데이터가 존재하지 않는 경우", function (done) {
+        request(app)
+            .get("/common/status")
+            .query({ UserId: 2 })
+            .set("Accept", "application/json")
+            .expect((res) => {
+                // console.log(res.body);
+                expect(res.body).toStrictEqual({
+                    status: 200,
+                    success: true,
+                    message: successMeta["SUC-COMMON-0001"].message,
+                    data: {
+                        nickname: "테스트",
+                        history: getHistory(new Date()),
+                        deposit: 10000000,
+                        containStockAsset: 0,
+                    },
+                });
+            })
+            .expect(200, done);
+    });
+    it("user id 존재하지 않고 Deposits에 데이터가 존재하지 않는 경우", function (done) {
+        request(app)
+            .get("/common/status")
+            .query({ UserId: 3 })
+            .set("Accept", "application/json")
+            .expect((res) => {
+                // console.log(res.body);
+                expect(res.body).toStrictEqual({
+                    status: 400,
+                    success: false,
+                    errorcode: "ERR-MAIN-0002-1",
+                    message: errorMeta["ERR-MAIN-0002-1"].message,
+                });
+            })
+            .expect(400, done);
+    });
+});
