2a49fbf8200f747d7e2e00ea2beb3b26e90dc972
JinLee <41279487+leejin21@users.noreply.github.com>
2022-05-18 20:47:42 +0900

    [Feature/wallet] wallet 화면 서버 수정 및 TODO 작성(작년 코드 머지) (#13)
    * feat(transactions): 지갑 화면 거래내역 가데이터 전송 <REST>
    
    * feat(transactions): 서비스 레이어 작성 및 테스트 코드 작성
    
    * fix(transactions): 테스트로 감지한 오류 수정
    
    * add(wallet): valuationLossRate 추가 및 수정 TODO 추가

--- modules/service-modules.js
+++ modules/service-modules.js
@@ -7,7 +7,7 @@ const getYesterdayThisMin = () => {
     let d = new Date();
     d.setDate(d.getDate() - 1);
     d.setSeconds(0);
-    d.setHours(d.getHours() - 4);
+    d.setHours(d.getHours());
     d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
     return d;
 };
@@ -16,13 +16,61 @@ const getYesterdayNextMin = () => {
     d.setDate(d.getDate() - 1);
     d.setMinutes(d.getMinutes() + 1);
     d.setSeconds(0);
-    d.setHours(d.getHours() - 4);
+    d.setHours(d.getHours());
     d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
     return d;
+};
+
+const getTheDate = (d) => {
+    /*
+    INPUT
+        new Date() 이 형식임
+    
+    OUTPUT 
+        '2021/10/02' format으로 return
+    */
+    // TODO 타임 존 문제 ...
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const kr_curr = new Date(d + KR_TIME_DIFF);
+    // console.log(kr_curr);
+    // console.log(kr_curr.toLocaleTimeString());
+    const origin_list = d.toLocaleDateString().split("/");
+    const thatDate =
+        origin_list[2] + "/" + origin_list[0] + "/" + origin_list[1];
+    return thatDate;
+};
+
+const getTheTime = (d) => {
+    /*
+    INPUT
+        new Date() 이 형식임
+    
+    OUTPUT 
+        '01:22' format으로 return
+        d.getDay().toString().padStart(2 ,'0')
+    */
+    // console.log(d.toLocaleTimeString());
+    const [origin_list, when] = d.toLocaleTimeString().split(" ");
+    const [hour, min, sec] = origin_list.split(":");
+    const hour24 = when === "PM" ? parseInt(hour) + 12 : parseInt(hour);
+    return hour24.toString().padStart(2, "0") + ":" + min;
+};
+
+const getOverlapRemovedList = (list) => {
+    const objUnique = {};
+
+    list.forEach((el) => {
+        objUnique[el] = true;
+    });
+    const arrUnique = Object.keys(objUnique);
+    return arrUnique;
 };
 
 module.exports = {
     convertUTCDateToLocalDate,
     getYesterdayThisMin,
     getYesterdayNextMin,
+    getTheDate,
+    getTheTime,
+    getOverlapRemovedList,
 };
